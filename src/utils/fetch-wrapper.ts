3684d1366fe7f9c726be0f335c6abfa7c79c5802
althafdaa <althafdemiandraakbar@gmail.com>
2024-07-03 08:12:45 +0700

    chore: minor refactor in fech wrapper

--- src/utils/fetch-wrapper.ts
+++ src/utils/fetch-wrapper.ts
@@ -4,7 +4,7 @@ import nookies from 'nookies';
 import { newTokenResponse } from '@/types/auth';
 import { z } from 'zod';
 
-const TOKEN_NEED_TO_REFRESH = ['TOKEN_INVALID', 'TOKEN_EXPIRED'];
+const TOKEN_NEED_TO_REFRESH = new Set(['TOKEN_INVALID', 'TOKEN_EXPIRED']);
 
 /**
  * Parses the error message and returns the error object if possible.
@@ -12,18 +12,17 @@ const TOKEN_NEED_TO_REFRESH = ['TOKEN_INVALID', 'TOKEN_EXPIRED'];
  * @returns The parsed error object or null if parsing fails.
  */
 export const parseError = (err: Error | null) => {
-  if (err) {
-    try {
-      return JSON.parse(err.message) as {
-        code: number;
-        error: string;
-        error_id: string;
-      };
-    } catch (e) {
-      return null;
-    }
+  if (!err) return null;
+
+  try {
+    return JSON.parse(err.message) as {
+      code: number;
+      error: string;
+      error_id: string;
+    };
+  } catch (e) {
+    return null;
   }
-  return null;
 };
 
 type RequestInitConfig = Omit<RequestInit, 'body'> & {
@@ -54,32 +53,31 @@ export const fetchInstance = async <T>(
   endpoint: string,
   requestInit?: RequestInitConfig,
 ) => {
-  const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
-  let queryString = '';
-  const accessToken = nookies.get(c).access_token;
-
   const {
     body,
     params,
     headers: customHeaders,
     ...customConfig
   } = requestInit || {};
+
   const headers: HeadersInit & {
     'Content-Type'?: string;
   } = {
     'Content-Type': 'application/json',
   };
-  const isFormData = body instanceof FormData;
 
+  const accessToken = nookies.get(c).access_token;
   if (accessToken) {
     headers.Authorization = `Bearer ${accessToken}`;
   }
 
+  let queryString = '';
   if (params) {
     queryString =
       '?' + QueryString.stringify(params, { arrayFormat: 'repeat' });
   }
 
+  const isFormData = body instanceof FormData;
   if (isFormData) {
     delete headers['Content-Type'];
   }
@@ -100,7 +98,10 @@ export const fetchInstance = async <T>(
     config.body = body;
   }
 
-  return fetch(baseURL + endpoint + queryString, config).then(async (res) => {
+  const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+  const url = baseURL + endpoint + queryString;
+
+  return fetch(url, config).then(async (res) => {
     if (res.ok) {
       return (await res.json()) as T;
     }
@@ -109,11 +110,7 @@ export const fetchInstance = async <T>(
     const errObj = new Error(errorMessage);
     const err = parseError(errObj);
 
-    if (
-      err &&
-      res.status === 401 &&
-      TOKEN_NEED_TO_REFRESH.includes(err.error)
-    ) {
+    if (err && res.status === 401 && TOKEN_NEED_TO_REFRESH.has(err.error)) {
       const refreshing = await fetch(baseURL + '/v1/auth/refresh', {
         ...config,
         method: 'GET',
@@ -132,20 +129,20 @@ export const fetchInstance = async <T>(
         return Promise.reject(refreshErrObj);
       }
 
-      const data = (await refreshing.json()) as z.infer<
+      const { data } = (await refreshing.json()) as z.infer<
         typeof newTokenResponse
       >;
 
-      nookies.set(c, 'access_token', data.data.access_token, {
+      nookies.set(c, 'access_token', data.access_token, {
         path: '/',
       });
 
       // Retry the original request with the new access token
       config.headers = {
         ...config.headers,
-        Authorization: `Bearer ${data.data.access_token}`,
+        Authorization: `Bearer ${data.access_token}`,
       };
-      const retryRes = await fetch(baseURL + endpoint + queryString, config);
+      const retryRes = await fetch(url, config);
       if (retryRes.ok) {
         return (await retryRes.json()) as T;
       }
