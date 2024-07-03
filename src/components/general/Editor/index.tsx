93a2299d49736401a6f784556650fce95ae6d180
althafdaa <althafdemiandraakbar@gmail.com>
2024-07-03 21:30:27 +0700

    feat: upload image in tiptap editor

--- src/components/general/Editor/index.tsx
+++ src/components/general/Editor/index.tsx
@@ -8,8 +8,14 @@ import { Level } from '@tiptap/extension-heading';
 import Image from '@tiptap/extension-image';
 import { useEffect, useState } from 'react';
 import LinkIcon from '@/icons/LinkIcon';
-import { Tooltip } from '@nextui-org/react';
+import { Popover, PopoverContent, PopoverTrigger } from '@nextui-org/react';
 import XCircleIcon from '@/icons/XCircleIcon';
+import Uploader from '../Uploader';
+import ImageIcon from '@/icons/ImageIcon';
+import { ACCEPTED_IMAGE_TYPES_SET, MEGABYTE } from '@/constants/common';
+import toast from 'react-hot-toast';
+import { prettifyError } from '@/utils/helper';
+import { uploadService } from '@/services/upload';
 
 const HEADING_LEVELS = [1, 2, 3] as Level[];
 
@@ -44,83 +50,14 @@ const extensions = [
 
 const LinkMenu = ({ editor }: { editor: Editor | null }) => {
   const [link, setLink] = useState('');
-  const [open, setOpen] = useState(false);
-
-  const handleOpen = (open: boolean) => {
-    const previousUrl = editor?.getAttributes('link')?.href;
-    if (previousUrl) {
-      setLink(previousUrl);
-    }
-
-    setOpen(open);
-  };
 
   if (!editor) {
     return null;
   }
 
   return (
-    <div className="flex gap-1">
-      <Tooltip
-        isOpen={open}
-        onOpenChange={(o) => {
-          handleOpen(o);
-        }}
-        placement="bottom-start"
-        content={
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              if (!link) {
-                editor
-                  .chain()
-                  .focus()
-                  .extendMarkRange('link')
-                  .unsetLink()
-                  .run();
-                return;
-              }
-              // update link
-              editor
-                .chain()
-                .focus()
-                .extendMarkRange('link')
-                .setLink({ href: link })
-                .run();
-            }}
-            className="flex gap-1"
-          >
-            <input
-              type="text"
-              className="rounded border border-neutral-500 p-1 px-2"
-              placeholder="Link"
-              value={link}
-              onChange={(e) => setLink(e.target.value.trim())}
-            />
-            <button
-              className="rounded border border-neutral-500 p-1"
-              type="submit"
-            >
-              {editor.isActive('link') ? 'Update' : 'Add'}
-            </button>
-            <button
-              type="button"
-              className="rounded border border-neutral-500 p-1"
-              onClick={() => {
-                editor
-                  .chain()
-                  .focus()
-                  .extendMarkRange('link')
-                  .unsetLink()
-                  .run();
-                setLink('');
-              }}
-            >
-              <XCircleIcon width={16} height={16} />
-            </button>
-          </form>
-        }
-      >
+    <Popover placement="bottom-start">
+      <PopoverTrigger>
         <button
           type="button"
           className={classNames(
@@ -129,20 +66,58 @@ const LinkMenu = ({ editor }: { editor: Editor | null }) => {
               ? 'bg-neutral-500 !text-white'
               : 'bg-white text-neutral-500',
           )}
-          onClick={() => {
-            handleOpen(!open);
-          }}
         >
           <LinkIcon width={16} height={20} />
         </button>
-      </Tooltip>
-    </div>
+      </PopoverTrigger>
+      <PopoverContent className="p-0">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (!link) {
+              editor.chain().focus().extendMarkRange('link').unsetLink().run();
+              return;
+            }
+            // update link
+            editor
+              .chain()
+              .focus()
+              .extendMarkRange('link')
+              .setLink({ href: link })
+              .run();
+          }}
+          className="flex gap-1"
+        >
+          <input
+            type="text"
+            className="rounded border border-neutral-500 p-1 px-2"
+            placeholder="Link"
+            value={link}
+            onChange={(e) => setLink(e.target.value.trim())}
+          />
+          <button
+            className="rounded border border-neutral-500 p-1"
+            type="submit"
+          >
+            {editor.isActive('link') ? 'Update' : 'Add'}
+          </button>
+          <button
+            type="button"
+            className="rounded border border-neutral-500 p-1"
+            onClick={() => {
+              editor.chain().focus().extendMarkRange('link').unsetLink().run();
+              setLink('');
+            }}
+          >
+            <XCircleIcon width={16} height={16} />
+          </button>
+        </form>
+      </PopoverContent>
+    </Popover>
   );
 };
 
 const HeadingsMenu = ({ editor }: { editor: Editor | null }) => {
-  const [open, setOpen] = useState(false);
-
   if (!editor) {
     return null;
   }
@@ -165,7 +140,7 @@ const HeadingsMenu = ({ editor }: { editor: Editor | null }) => {
                 !editor.can().chain().focus().toggleHeading({ level }).run()
               }
               onClick={() =>
-                editor.chain().focus().toggleHeading({ level }).run()
+                editor.chain().toggleHeading({ level: level }).run()
               }
             >
               Heading {level}
@@ -177,16 +152,8 @@ const HeadingsMenu = ({ editor }: { editor: Editor | null }) => {
   };
 
   return (
-    <div className="relative">
-      <Tooltip
-        placement="bottom-start"
-        classNames={{
-          content: 'p-0',
-        }}
-        isOpen={open}
-        onOpenChange={(o) => setOpen(o)}
-        content={<TooltipContent />}
-      >
+    <Popover placement="bottom-start">
+      <PopoverTrigger>
         <div
           className={classNames(
             'rounded border border-neutral-500 p-1 text-neutral-500',
@@ -198,8 +165,55 @@ const HeadingsMenu = ({ editor }: { editor: Editor | null }) => {
           {' '}
           Heading
         </div>
-      </Tooltip>
-    </div>
+      </PopoverTrigger>
+      <PopoverContent className="p-0">
+        <TooltipContent />
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+const ImageMenu = ({ editor }: { editor: Editor | null }) => {
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <Uploader
+      accept={Array.from(ACCEPTED_IMAGE_TYPES_SET).join(',')}
+      className={classNames(
+        'flex items-center rounded border border-neutral-500 bg-white p-1 text-neutral-500',
+      )}
+      customRequest={async (files) => {
+        try {
+          const file = files?.[0];
+          if (!file) return;
+
+          if (file.size > 2 * MEGABYTE) {
+            toast.error('Max file size is 2MB');
+            return;
+          }
+
+          if (!ACCEPTED_IMAGE_TYPES_SET.has(file.type)) {
+            toast.error(
+              'File type must be' +
+                Array.from(ACCEPTED_IMAGE_TYPES_SET).join(','),
+            );
+            return;
+          }
+          const { data } = await uploadService.uploadImage({
+            file,
+            type: 'profiles',
+          });
+
+          editor.chain().focus().setImage({ src: data, alt: file.name }).run();
+        } catch (error) {
+          toast.error(prettifyError(error as Error));
+        }
+      }}
+    >
+      <ImageIcon width={16} height={16} />
+    </Uploader>
   );
 };
 
@@ -265,6 +279,8 @@ const MenuBar = ({ editor }: { editor: Editor | null }) => {
         </button>
 
         <LinkMenu editor={editor} />
+
+        <ImageMenu editor={editor} />
       </div>
     </div>
   );
